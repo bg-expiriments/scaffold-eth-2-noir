d43dc0c5f81fecf8a3646991dc1c9dc17975c553
FilipHarald <filip.harald@gmail.com>
2023-06-29 16:33:31 +0200

    docs: added TODO for solidity-verifier import

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,5 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+// TODO: import verifier contract from noir
 
 /**
  * Deploys a contract named "YourContract" using the deployer account and
