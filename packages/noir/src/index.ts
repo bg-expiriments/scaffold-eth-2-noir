a7b4cde6d01bb55b01e4f873625fcd02c6d26f55
filip <filip.harald@gmail.com>
2023-06-29 16:29:26 +0200

    feat: getter for circuits-source code

--- packages/noir/src/index.ts
+++ packages/noir/src/index.ts
@@ -1,3 +1,24 @@
-export function getHelloWorld(): string {
-  return "Hello World!";
+import path from "path";
+import { readdir, readFile } from "fs/promises";
+
+const circuits: any = {};
+
+function getPath(fileName?: string) {
+  if (!fileName) {
+    return path.resolve(process.cwd(), "./circuits/src");
+  }
+  return path.resolve(process.cwd(), "./circuits/src", fileName);
+}
+// TODO: remove all this since we use public-folder in nextjs
+
+export async function setup() {
+  const circuitPaths = await readdir(getPath());
+  for (const p of circuitPaths) {
+    // console.log("🗒 path: ", p);
+    circuits[p] = await readFile(getPath(p), "utf8");
+  }
+}
+
+export async function getCircuits() {
+  return circuits;
 }
