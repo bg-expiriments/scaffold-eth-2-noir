c4ff6e9a55f8fb03d5693760ab3975f70bcab135
filip <filip.harald@gmail.com>
2023-06-30 10:21:34 +0200

    fixed it

--- packages/noir/scripts/export_circuits_src.ts
+++ packages/noir/scripts/export_circuits_src.ts
@@ -1,4 +1,3 @@
-//import initWasm, { compile } from "@noir-lang/noir_wasm";
 import { compile } from "@noir-lang/noir_wasm";
 import { initialiseResolver } from "@noir-lang/noir-source-resolver";
 
@@ -6,7 +5,7 @@ import path from "path";
 import { readFileSync, writeFileSync } from "fs";
 
 const TARGET_FILE = "../nextjs/generated/circuits.json";
-const CIRCUIT_PATH = "./circuits/src";
+const CIRCUIT_PATH = "./circuits/src/";
 const CIRCUIT_ROOTS = ["main.nr"];
 
 function fileResolverCallback(id: string) {
@@ -15,21 +14,19 @@ function fileResolverCallback(id: string) {
     const code = readFileSync(CIRCUIT_PATH + id, "utf8") as string;
     return code;
   } catch (err) {
-    console.error("❌ error when reading file: ", id);
+    console.error(`❌ error when reading file (${id}) with error: ${(err as Error).stack}\n`);
     throw err;
   }
 }
 
 async function exportAsJson() {
   initialiseResolver(fileResolverCallback);
-  //initWasm();
   const data: any = {};
   try {
     for (const fileName of CIRCUIT_ROOTS) {
       data[fileName] = await compile({
         entry_point: fileName,
       });
-      console.log(JSON.stringify(data[fileName]));
     }
   } catch (err) {
     console.error("Error while compiling:", (err as Error).stack);
