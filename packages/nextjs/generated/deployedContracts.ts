303122e878993f052f76c6bae09118a9ed31adf3
Filip Harald <filip@kryha.io>
2023-07-04 13:07:18 +0200

    feat: deploy solidity zk-verifiers with `yarn deploy`

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -4,8 +4,174 @@ const contracts = {
       chainId: "31337",
       name: "localhost",
       contracts: {
+        VerifierLessThen: {
+          address: "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82",
+          abi: [
+            {
+              inputs: [],
+              name: "EC_SCALAR_MUL_FAILURE",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "MOD_EXP_FAILURE",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "PROOF_FAILURE",
+              type: "error",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "expected",
+                  type: "uint256",
+                },
+                {
+                  internalType: "uint256",
+                  name: "actual",
+                  type: "uint256",
+                },
+              ],
+              name: "PUBLIC_INPUT_COUNT_INVALID",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "PUBLIC_INPUT_GE_P",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "PUBLIC_INPUT_INVALID_BN128_G1_POINT",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "getVerificationKeyHash",
+              outputs: [
+                {
+                  internalType: "bytes32",
+                  name: "",
+                  type: "bytes32",
+                },
+              ],
+              stateMutability: "pure",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "bytes",
+                  name: "_proof",
+                  type: "bytes",
+                },
+                {
+                  internalType: "bytes32[]",
+                  name: "_publicInputs",
+                  type: "bytes32[]",
+                },
+              ],
+              name: "verify",
+              outputs: [
+                {
+                  internalType: "bool",
+                  name: "",
+                  type: "bool",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+          ],
+        },
+        VerifierNotEq: {
+          address: "0x9A676e781A523b5d0C0e43731313A708CB607508",
+          abi: [
+            {
+              inputs: [],
+              name: "EC_SCALAR_MUL_FAILURE",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "MOD_EXP_FAILURE",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "PROOF_FAILURE",
+              type: "error",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "uint256",
+                  name: "expected",
+                  type: "uint256",
+                },
+                {
+                  internalType: "uint256",
+                  name: "actual",
+                  type: "uint256",
+                },
+              ],
+              name: "PUBLIC_INPUT_COUNT_INVALID",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "PUBLIC_INPUT_GE_P",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "PUBLIC_INPUT_INVALID_BN128_G1_POINT",
+              type: "error",
+            },
+            {
+              inputs: [],
+              name: "getVerificationKeyHash",
+              outputs: [
+                {
+                  internalType: "bytes32",
+                  name: "",
+                  type: "bytes32",
+                },
+              ],
+              stateMutability: "pure",
+              type: "function",
+            },
+            {
+              inputs: [
+                {
+                  internalType: "bytes",
+                  name: "_proof",
+                  type: "bytes",
+                },
+                {
+                  internalType: "bytes32[]",
+                  name: "_publicInputs",
+                  type: "bytes32[]",
+                },
+              ],
+              name: "verify",
+              outputs: [
+                {
+                  internalType: "bool",
+                  name: "",
+                  type: "bool",
+                },
+              ],
+              stateMutability: "view",
+              type: "function",
+            },
+          ],
+        },
         YourContract: {
-          address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+          address: "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0",
           abi: [
             {
               inputs: [
---
+++ packages/noir/scripts/export_sol_verifiers.sh
@@ -0,0 +1,8 @@
+rm ../hardhat/contracts/verifiers/*
+for d in circuits/*/ ; do
+  echo $d
+  cd $d
+  nargo codegen-verifier
+  mv contract/plonk_vk.sol ../../../hardhat/contracts/verifiers/$(basename $d).sol
+  cd ../..
+done
