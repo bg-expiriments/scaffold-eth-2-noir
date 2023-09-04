acc3d96aec716f8e1fc914eef7074a3e04b034dd
Filip Harald <Filip.harald@gmail.com>
2023-09-04 15:33:02 +0200

    ui: updated header (#35)

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Bars3Icon, BeakerIcon, BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -37,24 +37,15 @@ export const Header = () => {
   const navLinks = (
     <>
       <li>
-        <NavLink href="/">Home</NavLink>
-      </li>
-      <li>
         <NavLink href="/debug">
           <BugAntIcon className="h-4 w-4" />
           Debug Contracts
         </NavLink>
       </li>
       <li>
-        <NavLink href="/example-ui">
-          <SparklesIcon className="h-4 w-4" />
-          Example UI
-        </NavLink>
-      </li>
-      <li>
         <NavLink href="/example-zk">
-          <BeakerIcon className="h-4 w-4" />
-          Example UI ZK
+          <span className="h-4 w-4">🎈</span>
+          Age restriction with ZK
         </NavLink>
       </li>
       <li>
