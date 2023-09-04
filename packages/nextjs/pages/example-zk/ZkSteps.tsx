54f33f82dfede5550e577a6f721d92730770c6c7
urbbru <41080133+urbbru@users.noreply.github.com>
2023-09-04 10:18:13 +0200

    ui: added steps component (#30)

--- packages/nextjs/pages/example-zk/ZkSteps.tsx
+++ packages/nextjs/pages/example-zk/ZkSteps.tsx
@@ -8,18 +8,18 @@ import type { NextPage } from "next";
 
 const TEN_YEARS = 1000 * 60 * 60 * 24 * 365 * 10;
 const YEAR_TEN_YEARS_AGO = new Date(Date.now() - TEN_YEARS).getFullYear();
-const FIRST_STEP = 1;
-const LAST_STEP = 4;
+const INTRO_STEP = 0;
+const LAST_STEP = 3;
 
 export function renderZkSteps(currentStep: number, setCurrentStep: Dispatch<SetStateAction<number>>) {
   switch (currentStep) {
-    case 1:
+    case 0:
       return <ZkStepsIntro setCurrentStep={setCurrentStep} yearTenYearsAgo={YEAR_TEN_YEARS_AGO} />;
-    case 2:
+    case 1:
       return <BirthDateSignature aliceDefaultAge={YEAR_TEN_YEARS_AGO + 1} />;
-    case 3:
+    case 2:
       return <GenerateProof requiredBirthYear={YEAR_TEN_YEARS_AGO} />;
-    case 4:
+    case 3:
       return <AgeRestrictedContractExecutor />;
     default:
       return <></>;
@@ -27,34 +27,43 @@ export function renderZkSteps(currentStep: number, setCurrentStep: Dispatch<SetS
 }
 
 const ZkSteps: NextPage = () => {
-  const [currentStep, setCurrentStep] = useState(1);
+  const [currentStep, setCurrentStep] = useState(0);
+  const step2ClassName = currentStep >= 2 ? "step-primary" : "";
+  const step3ClassName = currentStep >= 3 ? "step-primary" : "";
+  const previousButtonDisabled = currentStep === INTRO_STEP;
 
   return (
     <>
       <SignedStats />
       {renderZkSteps(currentStep, setCurrentStep)}
-      <div className="join grid grid-cols-2 mt-8 gap-8">
-        {currentStep !== FIRST_STEP && (
-          <>
-            <button
-              className="join-item btn btn-outline"
-              onClick={() => setCurrentStep(currentStep => currentStep - 1)}
-              disabled={currentStep === FIRST_STEP}
-            >
-              Previous
-            </button>
-            {currentStep !== LAST_STEP && (
+      {currentStep !== INTRO_STEP && (
+        <>
+          <ul className="steps pt-12 steps-vertical md:steps-horizontal">
+            <li className="step step-primary">Third party signature 🏛📜</li>
+            <li className={`step ${step2ClassName}`}>Generate proof ✅</li>
+            <li className={`step ${step3ClassName}`}>Call contract🎈</li>
+          </ul>
+          <div className="join grid grid-cols-2 mt-8 gap-8">
+            <>
               <button
                 className="join-item btn btn-outline"
-                onClick={() => setCurrentStep(currentStep => currentStep + 1)}
-                disabled={currentStep === LAST_STEP}
+                onClick={() => setCurrentStep(currentStep => currentStep - 1)}
+                disabled={previousButtonDisabled}
               >
-                Next
+                Previous
               </button>
-            )}
-          </>
-        )}
-      </div>
+              {currentStep !== LAST_STEP && (
+                <button
+                  className="join-item btn btn-outline"
+                  onClick={() => setCurrentStep(currentStep => currentStep + 1)}
+                >
+                  Next
+                </button>
+              )}
+            </>
+          </div>
+        </>
+      )}
     </>
   );
 };
