d3b9933608e17d29246c50c624160fc5cce8d35c
Filip Harald <Filip.harald@gmail.com>
2023-09-04 15:32:35 +0200

    ui: changed logo (#34)

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -93,11 +93,11 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <Image alt="SE2 logo" className="cursor-pointer bg-white rounded-md" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Scaffold-ETH Noir</span>
+            <span className="text-xs">Showcasing zero-knowledge with Noir-lang</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
